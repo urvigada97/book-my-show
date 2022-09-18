d334f484505e65002642e0d9b2e8d2b64a2aba9f
Urvi Gadda <urvigada0@gmail.com>
2022-09-18 21:53:22 +0530

    Added heroku postgres

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "admin",
-  DB: "BookMyShow",
+  HOST: "ec2-3-219-52-220.compute-1.amazonaws.com",
+  USER: "dbiujzfhugfscs",
+  PASSWORD: "977134ce803ef8bf8d9bf2f7ec43a0a39d4a0a729715ce0d78ec830fd88eaa74",
+  DB: "d5tqldpd6tsg6k",
   dialect: "postgres",
   pool: {
     max: 5,
